577190174f22191296ad424242a8cac7dee1e56a
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-08-14 23:53:41 +0530

    started register function and chhanged the user model email constratints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,14 @@
+const Users = require('../models/userModel');
+
 const userController = {
-    register: (req, res) => {
-        res.json({msg: "Inside Register Again"});
+    register: async (req, res) => {
+        const {name, email, password} = req.body;
+
+        const user = await Users.findOne({email});
+
+        if(user) return res.status(400).json({msg: "User already exists"});
+
+
     }
 }
 
